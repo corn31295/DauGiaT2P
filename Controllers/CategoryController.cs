908cf1cee81fbd7d48da79eefdca94dac8cc05ec
corn31295 <ngotu31295@gmail.com>
2017-06-22 22:58:29 +0700

    Cat + Pro + Detail

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEAMT2P.Models;
+
+namespace TEAMT2P.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category/List
+        public ActionResult List()
+        {
+            using (var ctx = new QLBHEntities())
+            {
+                var list = ctx.Categories.ToList();
+                return PartialView("ListPartial", list);
+            }
+        }
+    }
+}
