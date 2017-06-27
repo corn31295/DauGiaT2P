1265a812b10cd5e54e83f114691a2b55cbde5aba
tvphuoc <tvphuoc1996@gmail.com>
2017-06-27 17:57:21 +0700

    Search

--- Controllers/MembersController.cs
+++ Controllers/MembersController.cs
@@ -10,64 +10,17 @@ namespace TEAMT2P.Controllers
 {
     public class MembersController : Controller
     {
-        // GET: Members//Edit 
+        // GET: Members
         public ActionResult Edit()
         {
-            User model = new Models.User();
-            model = CurrentContext.GetCurUser();
-            return View(model);
+            return View();
         }
 
         // POST: Members
         [HttpPost]
-        public ActionResult Edit(User model)
-        {
-            using (var ctx = new QLBHEntities())
-            {
-                string username = CurrentContext.GetCurUser().f_Username;
-                var user = ctx.Users
-                .Where(s => s.f_Username == username).FirstOrDefault();
-                if (user != null)
-                {
-                    user.f_Email = model.f_Email;
-                    user.f_Name = model.f_Name;
-                    user.f_Address = model.f_Address;
-                    user.f_DOB = model.f_DOB;
-                    ctx.SaveChanges();               
-                }
-            }
-            ViewBag.Msg = "Sửa thành công !";
-            return View(model);
-        }
-
-        // GET: Members//PasswordChange 
-        public ActionResult PasswordChange()
+        public ActionResult Edit(Register model)
         {
             return View();
         }
-
-        // POST: Members//PasswordChange 
-        [HttpPost]
-        public ActionResult PasswordChange(Password model)
-        {
-            string encPWD = StringUtils.Md5(model.RawPWD);
-            using (var ctx = new QLBHEntities())
-            {
-                var user = ctx.Users
-                    .Where(u => u.f_Password == encPWD).FirstOrDefault();
-                if (user != null)
-                {
-                    user.f_Password = StringUtils.Md5(model.NRawPWD);
-                    ctx.SaveChanges();
-                    ViewBag.Msg = "Đổi mật khẩu thành công !";
-                }
-                else
-                {
-                    ViewBag.ErrorMsg = "Mật khẩu cũ không đúng!!";
-                    return View();
-                }
-            }
-            return View();
-        }       
     }
 }
---
+++ Controllers/SearchController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TEAMT2P.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace TEAMT2P.Controllers
+{
+    public class SearchController : Controller
+    {
+        QLBHEntities db = new QLBHEntities();
+        [HttpPost]
+        // GET: Search
+        public ActionResult KetQuaTimKiem(FormCollection f, int? page)
+        {
+            string sTuKhoa = f["txtTimKiem"].ToString();
+            ViewBag.TuKhoa = sTuKhoa;
+            List<Product> lstKQTK = db.Products.Where(n => n.ProName.Contains(sTuKhoa)).ToList();
+            //Phân trang
+            int pageNumber = (page ?? 1);
+            int pageSize = 9;
+            if (lstKQTK.Count == 0)
+            {
+                ViewBag.ThongBao = "Không tìm thấy sản phẩm";
+                return View(db.Products.OrderBy(n => n.ProName).ToPagedList(pageNumber,pageSize));
+            }
+            ViewBag.ThongBao = "Đã tìm thấy" + lstKQTK.Count + "Kết quả!";
+            return View(lstKQTK.OrderBy(n => n.ProName).ToPagedList(pageNumber,pageSize));
+        }
+
+        //public ActionResult KetQuaTimKiem(FormCollection f, int? page)
+        //{
+        //    string sTuKhoa = f["txtTimKiem"].ToString();
+        //    List<Product> lstKQTK = db.Products.Where(n => n.ProName.Contains(sTuKhoa)).ToList();
+        //    //Phân trang
+        //    int pageNumber = (page ?? 1);
+        //    int pageSize = 9;
+        //    if (lstKQTK.Count == 0)
+        //    {
+        //        ViewBag.ThongBao = "Không tìm thấy sản phẩm";
+        //        return View(db.Products.OrderBy(n => n.ProName).ToPagedList(pageNumber, pageSize));
+        //    }
+        //    ViewBag.ThongBao = "Đã tìm thấy" + lstKQTK.Count + "Kết quả!";
+        //    return View(lstKQTK.OrderBy(n => n.ProName).ToPagedList(pageNumber, pageSize));
+        //}
+    }
+}
